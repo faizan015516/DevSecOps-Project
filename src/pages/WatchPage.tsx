cb2ca50b239f20ddcb474528c317c4648aff5a1f
faizan015516 <154252245+faizan015516@users.noreply.github.com>
2026-09-12 16:59:08 +0530

    Update video source URL to Mux test stream

--- src/pages/WatchPage.tsx
+++ src/pages/WatchPage.tsx
@@ -49,7 +49,7 @@ export function Component() {
         {
           // src: videoData?.video,
           // src: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8",
-          src: "https://cdn.bitmovin.com/content/assets/sintel/hls/playlist.m3u8",
+          src: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
           type: "application/x-mpegurl",
         },
       ],
