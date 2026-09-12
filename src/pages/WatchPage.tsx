be66ea9493e9d9baeda3fa2ebf23ae56e234b605
faizan015516 <154252245+faizan015516@users.noreply.github.com>
2026-09-12 15:24:48 +0530

    Update video URL to https://cdn.bitmovin.com/content/assets/sintel/hls/playlist.m3u8

--- src/pages/WatchPage.tsx
+++ src/pages/WatchPage.tsx
@@ -49,7 +49,7 @@ export function Component() {
         {
           // src: videoData?.video,
           // src: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8",
-          src: "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+          src: "https://cdn.bitmovin.com/content/assets/sintel/hls/playlist.m3u8",
           type: "application/x-mpegurl",
         },
       ],
